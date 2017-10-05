9d201ebbc49a4a88c85b2b7dd0fe5c89117e26b9
lornamariak <lorna1661maria@gmail.com>
2017-10-05 22:42:41 +0300

    Add files via upload

---
+++ day1.R
@@ -0,0 +1,13 @@
+x <- readline(prompt="Enter age: ")
+x <- as.integer(x)
+comp<-sample(1:100, 3, replace=TRUE)
+if (comp < x) {
+  print("high")
+} else 
+{
+  if (comp > x) {
+    print("low")
+  } 
+  else print(comp)
+}
+
